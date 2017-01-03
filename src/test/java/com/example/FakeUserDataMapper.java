93ffbf3eda459ac59178245e70410365521f2b1a
umehara <umehara@umeharas-MacBook-Pro.local>
2017-01-03 20:58:36 +0900

    backfills user repo validate test

--- src/test/java/com/example/FakeUserDataMapper.java
+++ src/test/java/com/example/FakeUserDataMapper.java
@@ -2,16 +2,21 @@ package com.example;
 
 import java.util.Optional;
 
+@SuppressWarnings("OptionalUsedAsFieldOrParameterType")
 public class FakeUserDataMapper implements UserDataMapper {
     Optional<User> findByUsername_returnValue;
-
+    String findByUsername_param_username;
     @Override
     public Optional<User> findByUsername(String username) {
+        findByUsername_param_username = username;
         return findByUsername_returnValue;
     }
 
+    Optional<User> validate_returnValue;
+    LoginCredentials validate_param_credentials;
     @Override
     public Optional<User> validate(LoginCredentials credentials) {
-        return null;
+        validate_param_credentials = credentials;
+        return validate_returnValue;
     }
 }
--- src/test/java/com/example/UserRepoTest.java
+++ src/test/java/com/example/UserRepoTest.java
@@ -1,22 +1,49 @@
 package com.example;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Optional;
 
+import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsEqual.equalTo;
 import static org.junit.Assert.assertThat;
 
 @SuppressWarnings("OptionalGetWithoutIsPresent")
 public class UserRepoTest {
+    private FakeUserDataMapper dataMapper;
+    private UserRepo repo;
+
+    @Before
+    public void setUp() throws Exception {
+        dataMapper = new FakeUserDataMapper();
+        repo = new UserRepo(dataMapper);
+    }
+
     @Test
-    public void test_find_returnsUser() throws Exception {
-        FakeUserDataMapper dataMapper = new FakeUserDataMapper();
+    public void test_findByUsername_returnsUser_onSuccess() throws Exception {
         dataMapper.findByUsername_returnValue = Optional.of(new User(1, "adam"));
-        UserRepo repo = new UserRepo(dataMapper);
+
 
         Optional<User> maybeUser = repo.findByUsername("adam");
 
+
+        assertThat(dataMapper.findByUsername_param_username, is("adam"));
+        User user = maybeUser.get();
+        assertThat(user.getId(), equalTo(1));
+        assertThat(user.getUsername(), equalTo("adam"));
+    }
+
+    @Test
+    public void test_validate_returnsUser_onSuccess() throws Exception {
+        dataMapper.validate_returnValue = Optional.of(new User(1, "adam"));
+        LoginCredentials credentials = new LoginCredentials("adam", "secret");
+
+
+        Optional<User> maybeUser = repo.validate(credentials);
+
+
+        assertThat(dataMapper.validate_param_credentials, is(credentials));
         User user = maybeUser.get();
         assertThat(user.getId(), equalTo(1));
         assertThat(user.getUsername(), equalTo("adam"));
