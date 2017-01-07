0edb97e83ea02c71c90e8187e667b09e06341247
umehara <umehara@umeharas-MacBook-Pro.local>
2017-01-07 22:15:57 +0900

    CLIENT: Backfills AppTest

---
+++ client/login-app-react/test/AppTest.js
@@ -0,0 +1,27 @@
+import expect from 'expect'
+import { shallow } from 'enzyme'
+import React from 'react'
+import { Link } from 'react-router'
+import localStorage from 'localStorage'
+import App from '../src/js/App'
+
+describe('App', () => {
+  it('renders header', () => {
+    let app = shallow(<App/>)
+
+    expect(app.find(Link).length).toBe(4)
+    expect(app.find(Link).nodes).toInclude(<Link to='/login'>Sign in</Link>)
+    expect(app.find(Link).nodes).toInclude(<Link to='/about'>About</Link>)
+    expect(app.find(Link).nodes).toInclude(<Link to='/dashboard'>Dashboard</Link>)
+    expect(app.find(Link).nodes).toInclude(<Link to='/users'>Users</Link>)
+  })
+
+  it('renders header with logout link if logged in', () => {
+    localStorage.token = 'token'
+
+    let app = shallow(<App/>)
+
+    expect(app.find(Link).length).toBe(4)
+    expect(app.find(Link).nodes).toInclude(<Link to='/logout'>Log out</Link>)
+  })
+})
