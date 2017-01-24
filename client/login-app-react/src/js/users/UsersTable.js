d545fd69ecfb403938625f5516bf7a9e8e019d5b
umehara <umehara@umeharas-MacBook-Pro.local>
2017-01-25 00:16:53 +0900

    Adds add button to usersTable

--- client/login-app-react/src/js/users/UsersTable.js
+++ client/login-app-react/src/js/users/UsersTable.js
@@ -5,6 +5,15 @@ import { Link } from 'react-router'
 class UsersTable extends React.Component {
   constructor(props) {
     super(props)
+
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleSubmit(e) {
+    e.preventDefault()
+
+    const username = this.refs.username.value
+    const password = this.refs.role.value
   }
 
   render() {
@@ -14,6 +23,7 @@ class UsersTable extends React.Component {
           <td>{user.id}</td>
           <td><Link className={user.username} to={`users/${user.username}`}>{user.username}</Link></td>
           <td>{user.role}</td>
+          <td></td>
         </tr>
       )
     })
@@ -25,10 +35,17 @@ class UsersTable extends React.Component {
             <th>ID</th>
             <th>Name</th>
             <th>Role</th>
+            <th></th>
           </tr>
         </thead>
         <tbody>
           {users}
+          <tr>
+            <td></td>
+            <td><input ref='username' placeholder='username'></input></td>
+            <td><input ref='role' placeholder='password'></input></td>
+            <td onClick={this.handleSubmit}>+</td>
+          </tr>
         </tbody>
       </table>
     )
